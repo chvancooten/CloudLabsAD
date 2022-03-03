46842f45da09acaee82dcb4be9422e6bae4e5c58
Cas van Cooten <chvancooten@gmail.com>
2022-03-03 22:02:48 +0100

    Tweak output variables

---
+++ Terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "region" {
+  value = azurerm_resource_group.cloudlabs-rg.region
+  description = "The region in which the resources are deployed. Based on the configured resource group."
+}
+
+output "public-ip" {
+  value = azurerm_public_ip.cloudlabs-ip.ip_address 
+  description = "The public IP address used to connect to the lab."
+}
+
+output "ip-whitelist" {
+    value = var.ip-whitelist
+    description = "The IP address(es) that are allowed to connect to the various lab interfaces."
+}
+
+output "public-ip-outbound" {
+    value = azurerm_public_ip.cloudlabs-ip-outbound.ip_address 
+    description = "The public IP address used by the lab machines to reach the internet."
+}
+
+output "ssh-user" {
+    value = var.ssh-user 
+    description = "The SSH username used to connect to the Debian machine."
+}
